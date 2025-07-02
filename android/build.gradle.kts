1308597cb0d8e812cbb58d40d22dd2481ba234f1
aimad moustajil <moustajil.dev@gmail.com>
2025-07-02 20:28:38 +0100

    initialization firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,22 @@
+// File: android/build.gradle.kts
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.8")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
+
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
